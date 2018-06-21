e4c516dd78e2f6c2902c83ad39118801738fd47c
Mibs Balsante <mibsbalsante@ckl.io>
2018-06-21 18:41:08 -0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './example'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './example'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
