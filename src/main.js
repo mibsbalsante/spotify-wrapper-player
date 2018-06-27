4ac3c0312e477a6714f2eefbc2d38566a375d8b1
Mibs Balsante <mibsbalsante@ckl.io>
2018-06-27 14:36:36 -0300

    Create album info and its tests

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Muse');
 const albumsList = document.querySelector('#album-list');
 
+const album = spotify.album.getAlbum('');
+const albumInfo = document.querySelector('#album-info');
+
 albums.then(data => renderAlbums(data.albums.items, albumsList));
+album.then(data => renderAlbumInfo(data, albumInfo));
