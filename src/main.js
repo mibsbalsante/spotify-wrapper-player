1d97bc798b8cd0aa31206e2fd60322e84aa34163
Mibs Balsante <mibsbalsante@ckl.io>
2018-06-27 16:16:43 -0300

    Dynamically fill album info when click in album list

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import spotify from './Spotify';
-import renderAlbumInfo from './AlbumInfo';
-import renderAlbumTracks from './AlbumTracks';
 import searchEnterTrigger from './SearchTrigger';
+import selectAlbumTrigger from './SelectAlbumTrigger';
 
 searchEnterTrigger();
-
-const album = spotify.album.getAlbum('');
-const albumInfo = document.querySelector('#album-info');
-const albumTracks = document.querySelector('#album-tracks');
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
--- tests/AlbumList.spec.js
+++ tests/AlbumList.spec.js
@@ -78,27 +78,27 @@ describe('AlbumList', () => {
   }];
 
   const markup = `
-    <div class="list-item">
-      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data[0].name}</p>
-        <p class="list-subtitle">${data[0].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data[0].id}">
+      <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data[0].artists[0].name}</p>
       </div>
     </div>`;
 
   const markup2 = `
-    <div class="list-item">
-      <img src="${data2[0].images[2].url}" alt="${data2[0].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data2[0].name}</p>
-        <p class="list-subtitle">${data2[0].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data2[0].id}">
+      <img src="${data2[0].images[2].url}" alt="${data2[0].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data2[0].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data2[0].artists[0].name}</p>
       </div>
     </div>
-    <div class="list-item">
-      <img src="${data2[1].images[2].url}" alt="${data2[1].name}" class="list-image">
-      <div class="list-description">
-        <p class="list-title">${data2[1].name}</p>
-        <p class="list-subtitle">${data2[1].artists[0].name}</p>
+    <div class="list-item" data-album-id="${data2[1].id}">
+      <img src="${data2[1].images[2].url}" alt="${data2[1].name}" class="list-image" data-album-id="${data[0].id}">
+      <div class="list-description" data-album-id="${data[0].id}">
+        <p class="list-title" data-album-id="${data[0].id}">${data2[1].name}</p>
+        <p class="list-subtitle" data-album-id="${data[0].id}">${data2[1].artists[0].name}</p>
       </div>
     </div>`;
 
