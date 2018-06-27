5363312d711f30d34c0dc6bf286e2c7935e030b0
Mibs Balsante <mibsbalsante@ckl.io>
2018-06-27 15:51:30 -0300

    Show converted time

--- tests/AlbumTracks.spec.js
+++ tests/AlbumTracks.spec.js
@@ -3,6 +3,7 @@
 import 'jsdom-global/register';
 import { expect } from 'chai';
 import renderAlbumTracks from '../src/AlbumTracks';
+import convertToHumanTime from '../src/ConvertToHumanTime';
 
 describe('AlbumTracks', () => {
   const data = [{
@@ -16,7 +17,7 @@ describe('AlbumTracks', () => {
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
       <p class="music-number">1</p>
       <p class="music-title">Around The World</p>
-      <p class="music-duration">238733</p>
+      <p class="music-duration">${convertToHumanTime(238733)}</p>
     </div>`;
 
   it('should create and append the markup given a correct data', () => {
